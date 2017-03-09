b3d86f49d8cf10deff617d19a8fb5d4c8ae98946
ValeSucci <valesuc98@gmail.com>
2017-03-09 01:03:36 -0400

    cierre de sesion desde el menu de tres puntitos

--- app/src/main/java/titaniumstudios/myapplication2/MenuActivity.java
+++ app/src/main/java/titaniumstudios/myapplication2/MenuActivity.java
@@ -1,12 +1,25 @@
 package titaniumstudios.myapplication2;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -20,6 +33,8 @@ public class MenuActivity extends AppCompatActivity {
     private static final int opcion1= 1;
     private static final int opcion2 = 2;
     private static final int opcion3 = 3;
+    private static final int opcion4 = 4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +77,63 @@ public class MenuActivity extends AppCompatActivity {
                 .setIcon(android.R.drawable.stat_notify_sdcard_prepare);
         menu.add(Menu.NONE, opcion3, Menu.NONE, "Detalles de la app")
                 .setIcon(android.R.drawable.ic_dialog_info);
+        menu.add(Menu.NONE, opcion4, Menu.NONE, "Cerrar Sesión")
+                .setIcon(android.R.drawable.ic_dialog_info);
         return true;
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case opcion4:
+                AlertDialog.Builder Dialogo = new AlertDialog.Builder(
+                        MenuActivity.this);
+
+                Dialogo.setTitle("Atención!");
+                Dialogo.setMessage("¿Seguro que desea cerrar sesión?");
+
+                Dialogo.setPositiveButton("Si",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                //cambiar los datos de MisPreferencias como pa q se borre
+
+                                //Abrimos el archivo de preferencias
+                                SharedPreferences prefs =
+                                        getSharedPreferences("MisPreferencias",Context.MODE_PRIVATE);
+
+                                //Editamos los campos existentes
+                                SharedPreferences.Editor editor = prefs.edit();
+                                editor.putString("usuario", "no");
+                                editor.putString("password", "no");
+                                //Concretamos la edicion
+                                editor.commit(); //pa guardar
+
+
+                                Intent intent = new Intent(context, MainActivity.class);
+                                finish();
+                                startActivity(intent);
+                            }
+                        });
+                Dialogo.setNegativeButton("No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(), "Ten mas cuidado con lo que presionas", Toast.LENGTH_SHORT).show();
+                                dialog.cancel();
+                            }
+                        });
+                Dialogo.show();
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+
+
+
 }
