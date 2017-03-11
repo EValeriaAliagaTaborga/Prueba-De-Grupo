79c91ffda8f40b7b9225667799c393b3bd99ad39
Rosa-Zambrana <ros.love.may0@gmail.com>
2017-03-11 17:35:27 -0400

    Perdon...

--- app/src/main/java/titaniumstudios/myapplication2/VerificacionDePedidoActivity.java
+++ app/src/main/java/titaniumstudios/myapplication2/VerificacionDePedidoActivity.java
@@ -3,11 +3,11 @@ package titaniumstudios.myapplication2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class VarificacionDePedidoActivity extends AppCompatActivity {
+public class VerificacionDePedidoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_varificacion_de_pedido);
+        setContentView(R.layout.activity_verificacion_de_pedido);
     }
 }
