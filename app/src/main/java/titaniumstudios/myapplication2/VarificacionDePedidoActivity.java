5c9f66c3adeda286c1de5ec59f47a1768522e1d0
Rosa-Zambrana <ros.love.may0@gmail.com>
2017-03-11 17:24:56 -0400

    Menu lateral

---
+++ app/src/main/java/titaniumstudios/myapplication2/VarificacionDePedidoActivity.java
@@ -0,0 +1,13 @@
+package titaniumstudios.myapplication2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VarificacionDePedidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_varificacion_de_pedido);
+    }
+}
