77ff3ea2967645f8be3d21fa1bf7806df769a985
Rosa-Zambrana <ros.love.may0@gmail.com>
2017-03-12 09:44:26 -0400

    hola :)

--- app/src/main/java/titaniumstudios/myapplication2/ArmaloActivity.java
+++ app/src/main/java/titaniumstudios/myapplication2/ArmaloActivity.java
@@ -1,11 +1,16 @@
 package titaniumstudios.myapplication2;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -31,6 +37,8 @@ public class ArmaloActivity extends AppCompatActivity {
     private ImageView imgPanInferior;
     private ImageView imgIngredienteElegido;
 
+    private boolean conCuenta = true;
+
     private double precio = 0.00;
     private TextView lblPrecio;
 
@@ -45,6 +53,11 @@ public class ArmaloActivity extends AppCompatActivity {
 
     private FloatingActionButton btnfCarrito;
 
+    private static final int opcion1= 1;
+    private static final int opcion2 = 2;
+    private static final int opcion3 = 3;
+    private static final int opcion4 = 4;
+
     //Lista para pasar datos
     ArrayList<String> datosDePedido = new ArrayList<String>();
 
@@ -52,6 +65,9 @@ public class ArmaloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_armalo);
+
+        Intent d=getIntent();
+        conCuenta = d.getBooleanExtra("con_cuenta", true);
 
         imgPanSuperior = (ImageView) findViewById(R.id.imgPanSuperior);
         imgCarneDeRes = (ImageView) findViewById(R.id.imgCarneRes);
@@ -619,5 +635,70 @@ public class ArmaloActivity extends AppCompatActivity {
             }
 
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if(conCuenta) {
+            menu.add(Menu.NONE, opcion1, Menu.NONE, "Perfil")
+                    .setIcon(android.R.drawable.ic_menu_add);
+            menu.add(Menu.NONE, opcion2, Menu.NONE, "Historial")
+                    .setIcon(android.R.drawable.stat_notify_sdcard_prepare);
+        }
+        menu.add(Menu.NONE, opcion3, Menu.NONE, "Detalles de la app")
+                .setIcon(android.R.drawable.ic_dialog_info);
+        if(conCuenta) {
+            menu.add(Menu.NONE, opcion4, Menu.NONE, "Cerrar Sesión")
+                    .setIcon(android.R.drawable.ic_dialog_info);
+        }
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case opcion4:
+                AlertDialog.Builder Dialogo = new AlertDialog.Builder(
+                        ArmaloActivity.this);
+
+                Dialogo.setTitle("Atención!");
+                Dialogo.setMessage("¿Seguro que desea cerrar sesión?");
+
+                Dialogo.setPositiveButton("Si",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                //cambiar los datos de MisPreferencias como pa q se borre
+
+                                //Abrimos el archivo de preferencias
+                                SharedPreferences prefs =
+                                        getSharedPreferences("MisPreferencias",Context.MODE_PRIVATE);
+
+                                //Editamos los campos existentes y en este caso borramos la cuenta existente
+                                SharedPreferences.Editor editor = prefs.edit();
+                                editor.putString("usuario", "no");
+                                editor.putString("password", "no");
+                                //Concretamos la edicion
+                                editor.commit(); //pa guardar
+
+
+                                Intent intent = new Intent(context, MainActivity.class);
+                                finish();
+                                startActivity(intent);
+                            }
+                        });
+                Dialogo.setNegativeButton("No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(), "Ten mas cuidado con lo que presionas", Toast.LENGTH_SHORT).show();
+                                dialog.cancel();
+                            }
+                        });
+                Dialogo.show();
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
